fa07574a368fda26cd59b06b4f6dd617734ef387
Syed Zulqarnain <syed@zulqarnain.dev>
2024-08-03 22:27:22 +0500

    update fix

--- src/modules/SpaceModule/segments/space/index.tsx
+++ src/modules/SpaceModule/segments/space/index.tsx
@@ -12,6 +12,7 @@ export default function Space() {
   const [sec, setSec] = useState(0);
   const [seeking, setSeeking] = useState(false);
   const [playing, setPlaying] = useState(true);
+  const [pause, setPause] = useState(true);
   const [isOwner, setIsOwner] = useState(false);
   const [previousUpdate, setPreviousUpdate] = useState(0);
   const userId = useUserId();
@@ -36,20 +37,20 @@ export default function Space() {
       if (data?.spaces?.[0]?.user_id === userId) {
         setIsOwner(true);
       }
-      if (!isOwner) {
-        if (data?.spaces?.[0]?.pause) {
+      if (data?.spaces?.[0]?.pause) {
+        if (!isOwner) {
           setPlaying(false);
-        } else {
+        }
+      } else {
+        if (!isOwner) {
           setPlaying(true);
         }
       }
-      console.log("space", data?.spaces?.[0], playing);
     }
   }, [data]);
 
   const handleProgress = async (state: any) => {
     console.log("playing", state, playerRef);
-    playerRef.current?.player?.handlePlay();
     if (!seeking) {
       if (isOwner) {
         const res: any = await update({
@@ -62,6 +63,12 @@ export default function Space() {
           },
         });
       } else {
+        // if (state.played > sec) {
+        //   setPlaying(false);
+        // } else {
+        //   setPlaying(true);
+        // }
+        // // Multiply by 100 to shift two decimal places to the left
         const updatedSec = sec;
         let statePlayed = state.played * 100;
         statePlayed = Math.floor(statePlayed);
@@ -70,12 +77,26 @@ export default function Space() {
         console.log("sec", statePlayed, updatedSecc);
         if (updatedSecc !== statePlayed) {
           setPreviousUpdate(updatedSec);
-          playerRef.current.seekTo(updatedSec);
+          playerRef.current.seekTo(updatedSec); // Convert milliseconds to seconds
+          //   if (previousUpdate === updatedSec) {
+          //     setPlaying(false);
+          //   }
         }
       }
+      // Multiply by 100 to shift two decimal places to the left
+      //   let statePlayed = state.played * 100;
+      //   statePlayed = Math.floor(statePlayed);
+      //   let updatedSecc = updatedSec * 100;
+      //   updatedSecc = Math.floor(updatedSecc);
+      //   console.log("sec", statePlayed, updatedSecc);
+      //   if (updatedSecc !== statePlayed) {
+      //     playerRef.current.seekTo(updatedSec); // Convert milliseconds to seconds
+      //   }
+      // footballChannel.publish("seconds", { sec: state.played });
     }
   };
 
+  console.log("dataS", data);
   return (
     <div className="flex justify-center items-center">
       <div className="max-w-[600px] ">
