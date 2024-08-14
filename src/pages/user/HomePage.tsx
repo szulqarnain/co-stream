1dcf5372eb3269b457d35cbbfb8a34d06afc49cb
Syed Zulqarnain <syed@zulqarnain.dev>
2024-08-14 23:59:29 +0500

    update

--- src/pages/user/HomePage.tsx
+++ src/pages/user/HomePage.tsx
@@ -1,18 +1,28 @@
+import { useSignOut } from "@nhost/react";
 import React from "react";
 import { Link } from "react-router-dom";
 import Spaces from "~/modules/SpaceModule/segments/spaces";
 
 export default function HomePage() {
+  const { signOut } = useSignOut();
   return (
     <div>
       <Spaces />
-      <div className=" bottom-0 fixed flex justify-center w-full bg-[#291248] pt-4">
+      <div className=" bottom-0 fixed flex justify-center w-full bg-[#291248] pt-4 gap-4 ">
         <Link
           to="/user/create"
           className="py-4 border border-1 border-[white] text-white rounded-lg px-4 mb-4 sm:mb-[40px]"
         >
           Create Space
         </Link>
+        <button
+          onClick={() => {
+            signOut();
+          }}
+          className="py-4 border border-1 border-[white] text-white rounded-lg px-4 mb-4 sm:mb-[40px]"
+        >
+          Sign Out
+        </button>
       </div>
     </div>
   );
